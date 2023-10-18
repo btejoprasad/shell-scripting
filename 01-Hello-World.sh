1680208c6132bec3f2f034db7a54c3b679f117d4
tejo <btejoprasad@gmail.com>
2023-10-18 10:49:37 -0700

    Learning Script

--- 01-Hello-World.sh
+++ 01-Hello-World.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Hi This is my First script which will be uploaded to GitHub Bye!."
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh: Hi How are you Suresh"
-echo "Suresh: I am doing good Ramesh"
-echo "Ramesh: Good Morning Suresh"
-echo "Suresh: Good Morning Ramesh"
+echo "$PERSON1: Hi How are you $PERSON2"
+echo "$PERSON2: I am doing good $PERSON1"
+echo "$PERSON1: Good Morning $PERSON2"
+echo "$PERSON2: Good Morning $PERSON1"
 
