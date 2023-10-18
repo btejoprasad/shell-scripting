bbf5a883c0bce0ea5cfdae36c4c7fee1744be940
tejo <btejoprasad@gmail.com>
2023-10-18 10:46:47 -0700

    Learning Script

--- 01-Hello-World.sh
+++ 01-Hello-World.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Hi This is my First script which will be uploaded to GitHub Bye!."
+
+echo "Ramesh: Hi How are you Suresh"
+echo "Suresh: I am doing good Ramesh"
+echo "Ramesh: Good Morning Suresh"
+echo "Suresh: Good Morning Ramesh"
+
