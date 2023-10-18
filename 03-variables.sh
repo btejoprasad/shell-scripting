6e96b065aacb044993fbe5531aa7d34c127fecf9
tejo <btejoprasad@gmail.com>
2023-10-18 11:02:36 -0700

    Learning Script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hi This is my First script which will be uploaded to GitHub Bye!."
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi How are you $PERSON2"
+echo "$PERSON2: I am doing good $PERSON1"
+echo "$PERSON1: Good Morning $PERSON2"
+echo "$PERSON2: Good Morning $PERSON1"
+
