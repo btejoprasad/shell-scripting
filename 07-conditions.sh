6534add42b1187cfd6190c5e99733bd3610ef6fc
tejo <btejoprasad@gmail.com>
2023-10-18 11:42:56 -0700

    Learning Script

---
+++ 07-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#check if the number is greater than 10 or not
+Number=$1
+
+if [ $Number -gt 10 ]
+then
+    echo "entered number $Number is greater than 10"
+Else
+    echo "Entered number $Number is not greater than 10"
+fi
