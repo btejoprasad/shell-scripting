71b20aed33797bc28b80c3fac489516ec51c7715
tejo <btejoprasad@gmail.com>
2023-10-18 11:19:52 -0700

    Learning Script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter your username"
 
-read USERNAME #this is the variable where the value of the one that user will enter
+read -s USERNAME #this is the variable where the value of the one that user will enter
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
 
-read PASSWORD
+read -s PASSWORD #-s will not show the text while typing the input
 echo "Password entered is: $PASSWORD"
