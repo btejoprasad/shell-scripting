640dac97580cebdda372a3206f26de07e8c17f85
tejo <btejoprasad@gmail.com>
2023-10-18 11:28:24 -0700

    Learning Script

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERSONS=("Tejo" "Lavanya" "Kishan")
-places=("Los Angeles" "Tirupati")
+places=("Los Angeles" "Tirupati") # small letters for variables will also work
 echo "First Person is: ${PERSONS[0]}"
 echo "All Persons: ${PERSONS[@]}"
 echo "All Places: ${places[@]}"
