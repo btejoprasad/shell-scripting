03955b5041c0f341cf4a188d8527193d76075ce1
tejo <btejoprasad@gmail.com>
2023-10-18 11:18:08 -0700

    Learning Script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "PLease enter your username"
+echo "Please enter your username"
 
 read USERNAME #this is the variable where the value of the one that user will enter
 echo "Username entered is: $USERNAME"
 
+echo "Please enter your password"
+
+read PASSWORD
+echo "Password entered is: $PASSWORD"
